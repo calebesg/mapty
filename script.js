d3bd8b65e221882b85b9be4a8f272b61bcb4bc10
CalebeSG <calebeguimaraes10@gmail.com>
2022-04-18 11:56:16 -0300

    render form

--- script.js
+++ script.js
@@ -11,30 +11,20 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 const renderMap = function (coords) {
-  const map = L.map('map').setView(coords, 16);
+  map = L.map('map').setView(coords, 16);
 
   L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
-  map.on('click', mapEvent => {
-    const { lat, lng } = mapEvent.latlng;
-
-    const popup = {
-      maxWidth: 250,
-      minWidth: 100,
-      autoClose: false,
-      closeOnClick: false,
-      className: 'running-popup',
-    };
-
-    L.marker([lat, lng])
-      .addTo(map)
-      .bindPopup(L.popup(popup))
-      .setPopupContent('Working')
-      .openPopup();
+  map.on('click', event => {
+    mapEvent = event;
+    form.classList.remove('hidden');
+    inputDistance.focus();
   });
 };
 
@@ -47,3 +37,31 @@ if (navigator.geolocation) {
     () => alert('Could not get your position.')
   );
 }
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const { lat, lng } = mapEvent.latlng;
+
+  const popup = {
+    maxWidth: 250,
+    minWidth: 100,
+    autoClose: false,
+    closeOnClick: false,
+    className: 'running-popup',
+  };
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(L.popup(popup))
+    .setPopupContent('Working')
+    .openPopup();
+
+  form.classList.add('hidden');
+  inputDistance.value = inputCadence.value = inputDuration.value = '';
+});
+
+inputType.addEventListener('change', e => {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
