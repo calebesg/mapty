5715dee89ad89b5b8153f7662e8d6a1cc1bc93cf
CalebeSG <calebeguimaraes10@gmail.com>
2022-04-18 10:17:45 -0300

    map rendered at the current position

--- script.js
+++ script.js
@@ -11,11 +11,25 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+const renderMap = function (coords) {
+  const map = L.map('map').setView(coords, 15);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker(coords)
+    .addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+};
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     position => {
       const { latitude, longitude } = position.coords;
-      console.log(latitude, longitude);
+      renderMap([latitude, longitude]);
     },
     () => alert('Could not get your position.')
   );
