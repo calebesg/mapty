208b9af7ed625008c7281fbd766fa5be990805b6
CalebeSG <calebeguimaraes10@gmail.com>
2022-04-19 15:41:15 -0300

    create new workout

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ class Workout {
 }
 
 class Running extends Workout {
+  type = 'running';
+
   constructor(coords, duration, distance, cadence) {
     super(coords, duration, distance);
     this.cadence = cadence;
@@ -35,6 +37,8 @@ class Running extends Workout {
 }
 
 class Cycling extends Workout {
+  type = 'cycling';
+
   constructor(coords, duration, distance, elevationGain) {
     super(coords, duration, distance);
     this.elevationGain = elevationGain;
@@ -46,11 +50,22 @@ class Cycling extends Workout {
   }
 }
 
+class Validadion {
+  static inputs(...inputs) {
+    return inputs.every(inp => Number.isFinite(inp));
+  }
+
+  static values(...values) {
+    return values.every(value => value > 0);
+  }
+}
+
 ////////////////////////////////////////////////////////////
 // APLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
+  #workouts = [];
 
   constructor() {
     this._getPosition();
@@ -95,23 +110,62 @@ class App {
     event.preventDefault();
 
     const { lat, lng } = this.#mapEvent.latlng;
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
 
+    let workout;
+
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+
+      if (
+        Validadion.inputs(distance, duration, cadence) === false ||
+        Validadion.values(distance, duration, cadence) === false
+      )
+        return alert('Input have to be positive numbers!');
+
+      workout = new Running([lat, lng], duration, distance, cadence);
+    }
+
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+
+      if (
+        Validadion.inputs(distance, duration, elevation) === false ||
+        Validadion.values(distance, duration) === false
+      )
+        return alert('Input have to be positive numbers!');
+
+      workout = new Cycling([lat, lng], duration, distance, elevation);
+    }
+
+    this.#workouts.push(workout);
+    this.renderMarker(workout);
+
+    inputDistance.value =
+      inputCadence.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
+
+    form.classList.add('hidden');
+  }
+
+  renderMarker(workout) {
     const popup = {
       maxWidth: 250,
       minWidth: 100,
       autoClose: false,
       closeOnClick: false,
-      className: 'running-popup',
+      className: `${workout.type}-popup`,
     };
 
-    L.marker([lat, lng])
+    L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(L.popup(popup))
       .setPopupContent('Working')
       .openPopup();
-
-    form.classList.add('hidden');
-    inputDistance.value = inputCadence.value = inputDuration.value = '';
   }
 }
 
